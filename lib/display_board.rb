79e424216d5e6b1aa0eecd5ba7b15a37b2e90fac
Tyson Cadorette <tyson.cadorette@wework.com>
2018-02-17 17:26:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 def display_board(board)
-  puts " "#{board[0]} "|   |   
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end  
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+end 
 
 
   
